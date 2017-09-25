2b02baf6b27341a6a50b0e5079b9dfd4510c90ae
HannaSokolsky <hannasokolsky@gmail.com>
2017-09-24 18:32:41 -0700

    changed chrom path

--- src/main/java/core/CrossBrowser.java
+++ src/main/java/core/CrossBrowser.java
@@ -45,7 +45,7 @@ public class CrossBrowser {
 	                 System.out.println("identified driver = " + driverPath);
 	                }
 	        else if  ((browser .equals("Chrome")) && (System.getProperty("os.name").toUpperCase().contains("LINUX")) ) {
-       		 driverPath = "resources/webdrivers/lnx/chromedriver32";
+       		 driverPath = "resources/webdrivers/lnx/chromedriver";
        		
                 System.out.println("identified driver = " + driverPath);
                }
